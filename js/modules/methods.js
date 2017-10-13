bd0c0b2be0c06b104437bf2eaa230314d909b384
trvswgnr <trvswgnr@gmail.com>
2017-10-13 15:50:00 -0400

    test method

--- js/modules/methods.js
+++ js/modules/methods.js
@@ -4,6 +4,9 @@ import classes from './functions/merge-classes';
  * Add independent methods / functions here.
  */
 export const methods = {
+  test(){
+    return 'test';
+  },
   mergeClasses(baseClass, ...mixins) {
     return classes(baseClass, ...mixins);
   }
